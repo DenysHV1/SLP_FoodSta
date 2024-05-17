97f7713be25ad0b8b5a309051a2d7a7c5b23fd06
DenVik <denyagarkusha2@gmail.com>
2024-05-17 19:16:27 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,35 +1,14 @@
-const burgerMenuButton=document.querySelector('.sta-burger-menu');
-const closeMenu=document.querySelector('.close-menu');
-const closeMenuHome=document.querySelector('.close__link-1');
-const closeMenuAbout=document.querySelector('.close__link-2');
-const closeMenuService=document.querySelector('.close__link-3');
-const closeMenuMenu=document.querySelector('.close__link-4');
-const closeMenuContact=document.querySelector('.close__link-5');
+const burgerMenuButton=document.getElementById('open-menu');
+const closeMenu=document.getElementById('close-menu');
 
-const containerMenu=document.querySelector('.burger-menu-container');
 
-// open 
+const containerMenu=document.getElementById('media-container');
+
 burgerMenuButton.addEventListener('click', ()=>{
-	containerMenu.classList.toggle('menu-is-open');
-	});
-// close
-closeMenu.addEventListener('click', ()=>{
-	containerMenu.remove('menu-is-open');
+	containerMenu.classList.add('is-open');
 	});
 
-closeMenuHome.addEventListener('click', ()=>{
-containerMenu.remove('menu-is-open');
-});
-closeMenuAbout.addEventListener('click', ()=>{
-containerMenu.remove('menu-is-open');
-});
-closeMenuService.addEventListener('click', ()=>{
-containerMenu.remove('menu-is-open');
-});
-closeMenuMenu.addEventListener('click', ()=>{
-containerMenu.remove('menu-is-open');
-});
-closeMenuContact.addEventListener('click', ()=>{
-containerMenu.remove('menu-is-open');
+closeMenu.addEventListener('click', ()=>{
+	containerMenu.classList.remove('is-open');
 });
 
