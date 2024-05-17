d851852538e15398de49ccd3669c8acbe597bc25
DenVik <denyagarkusha2@gmail.com>
2024-05-17 20:29:19 +0200

    word

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@ const burgerMenuButton=document.getElementById('open-menu');
 const closeMenu=document.getElementById('close-menu');
 
 
+// const closeMenu1=document.getElementById('close__link-1');
+// const closeMenu2=document.getElementById('close__link-2');
+// const closeMenu3=document.getElementById('close__link-3');
+// const closeMenu4=document.getElementById('close__link-4');
+// const closeMenu5=document.getElementById('close__link-5');
+
+
 const containerMenu=document.getElementById('media-container');
 
 burgerMenuButton.addEventListener('click', ()=>{
@@ -11,4 +18,20 @@ burgerMenuButton.addEventListener('click', ()=>{
 closeMenu.addEventListener('click', ()=>{
 	containerMenu.classList.remove('is-open');
 });
+
+// closeMenu1.addEventListener('click', ()=>{
+// 	containerMenu.classList.remove('is-open');
+// });
+// closeMenu2.addEventListener('click', ()=>{
+// 	containerMenu.classList.remove('is-open');
+// });
+// closeMenu3.addEventListener('click', ()=>{
+// 	containerMenu.classList.remove('is-open');
+// });
+// closeMenu4.addEventListener('click', ()=>{
+// 	containerMenu.classList.remove('is-open');
+// });
+// closeMenu5.addEventListener('click', ()=>{
+// 	containerMenu.classList.remove('is-open');
+// });
 
